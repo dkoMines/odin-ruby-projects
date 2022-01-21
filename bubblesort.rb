ea0959bfd05328c96d6ce753f3b7f0037911eef8
dkoMines <dko@mines.edu>
2022-01-21 11:50:38 -0700

    Slightly more efficient bubble sort

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,14 +1,13 @@
 def bubble_sort array
-    sorted = false
-    until sorted do
-        sorted = true
-        (0...array.length-1).each do |idx|
+    sorted = array.length-1
+    until sorted==0 do
+        (0...sorted).each do |idx|
             if array[idx] > array[idx+1]
                 # Swap idx and idx+1
                 tmp = array[idx]
                 array[idx] = array[idx+1]
                 array[idx+1] = tmp
-                sorted = false
+                sorted = idx
             end
         end
     end
